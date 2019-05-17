534a4d3bcef9f3ce433fcfd3afe96ac8aaf4977d
Prox <atcav1@outlook.com>
2019-05-17 16:31:49 +1000

    problem

--- post.pro.php
+++ post.pro.php
@@ -82,10 +82,9 @@ if (array_key_exists("user", $_SESSION) & !empty($_GET['TID']) & !empty($_GET['P
              <div>
                <form id="Thread" class="login-form hidden" action="./mydb/reply/creatReply.db.php" method="POST" role="form">
                  <?php
-                 $img = DB::query("SELECT UIMG FROM clientprofile WHERE CPID =".$_SESSION['cpid']);
+                 $img = DB::query("SELECT UIMG FROM clientProfile WHERE CPID =".$_SESSION['cpid']);
                  foreach ($img as $row) {
                    $uimg = $row['UIMG'];
-                 }
                  ?>
                  <div class="row border" style="padding: 10px;">
                    <img src="./img/<?php if (!empty($uimg)) {echo $uimg;} else { echo "Flat%20Gradient%20Social%20Media%20Icons/80/500px%20icon.png";} ?>" class="img-thumbnail" style="height: 80px; width: 80px;" alt="User Profile Image">
@@ -97,6 +96,7 @@ if (array_key_exists("user", $_SESSION) & !empty($_GET['TID']) & !empty($_GET['P
                      <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Submit</button>
                    </div>
                  </div>
+                 <?php } ?>
                </form>
              </div>
             </div>
