3241c306c636cff0381dcc023346f8b62d6efead
ProxayFox <atcav1@outlook.com>
2019-05-17 13:49:08 +1000

    IT'S ALIVE

--- post.pro.php
+++ post.pro.php
@@ -43,9 +43,41 @@ if (array_key_exists("user", $_SESSION) & !empty($_GET['TID']) & !empty($_GET['P
             }}
             ?>
           </div>
+          <br>
           <div class="row">
            <div class="col-9">
-             <h1>posting area</h1>
+             <?php
+              $reply = DB::query("SELECT * FROM reply INNER JOIN clientprofile ON reply.CPID = clientprofile.CPID INNER JOIN login ON clientprofile.CPID = login.CPID WHERE reply.PID = ".$PID." ORDER BY reply.RID ASC");
+              foreach ($reply as $row) {
+                $img = $row['UIMG'];
+                $userName = $row['UName'];
+                $content = $row['content'];
+                $date = $row['created'];
+
+             ?>
+             <div class="border container" style="margin-bottom: 10px;">
+               <div style="padding-top: 10px; padding-bottom: 10px;">
+                 <div class="row">
+                   <div class="col-12">
+                     <div class="row">
+                       <img src="./img/<?php echo $img; ?>" alt="User profile img" class="thumbnail" style="width: 80px; height: 80px;">
+                       <div style="padding-left: 10px;">
+                         <h3 style="padding-top: 10px;"><?php echo $userName; ?></h3>
+                         <i>&nbsp-&nbsp<?php echo $date; ?></i>
+                       </div>
+                     </div>
+                   </div>
+                   <div class="row">
+                     <div class="col-12">
+                      <p><?php echo $content; ?></p>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <?php
+              }
+             ?>
              <br>
              <div>
                <form id="Thread" class="login-form hidden" action="./mydb/reply/creatReply.db.php" method="POST" role="form">
@@ -55,7 +87,7 @@ if (array_key_exists("user", $_SESSION) & !empty($_GET['TID']) & !empty($_GET['P
                    $uimg = $row['UIMG'];
                  }
                  ?>
-                 <div class="row">
+                 <div class="row border" style="padding: 10px;">
                    <img src="./img/<?php if (!empty($uimg)) {echo $uimg;} else { echo "Flat%20Gradient%20Social%20Media%20Icons/80/500px%20icon.png";} ?>" class="img-thumbnail" style="height: 80px; width: 80px;" alt="User Profile Image">
                    <label for="reply"></label>
                    <div style="margin-top: 10px; margin-left: 5px;">
@@ -87,3 +119,5 @@ if (array_key_exists("user", $_SESSION) & !empty($_GET['TID']) & !empty($_GET['P
   exit;
 }
 ?>
+
+
