1b9ff06e0f8d17c4e432a1ec42a2080794f9de3b
Prox <atcav1@outlook.com>
2019-05-21 09:52:55 +1000

    signup sytem kinda working

--- mydb/register/o-register.db.php
+++ mydb/register/o-register.db.php
@@ -1,33 +1,53 @@
 <?php
-	require_once("../databaseManager/o-db.php");
+  require_once('../databaseManager/meekrodb.2.3.class.php');
+//  require_once('../databaseManager/o-db.php');
+  DB::$user = 'localhost';
+  DB::$dbName = 'forum';
+  DB::$user = 'root';
+  DB::$password = '';
+//	require_once("../databaseManager/o-db.php");
 	if (isset($_POST)) {
 		$uname = $_POST['uname'];
 		$email = $_POST['email'];
 		$pwd = $_POST['PWD'];
+		//hashing the password using sha512
+    $hashedPWD = hash('sha512', $pwd);
 		
 		// Add Users
-		$result = myDB::getInstance()->addUser($email);
-		if (!$result and $result1) {
+		$result = DB::insert('clientProfile', array(
+		  'CPID' => NULL,
+      'email' => $email
+    ));
+        //myDB::getInstance()->addUser($email);
+		if (!$result) {
 			// User was not created
 			// Duplicate Username
-			if (myDB::getInstance()->getUserByEmail($email)) {
-				header("Location: ../../signup.php?signup=email");
-			} elseif (myDB::getInstance()->getUserByUsername($uname)) {
-				header("Location: ../../signup.php?signup=username");
-			} else{
-				header("Location: ../../signup.php?signup=fail");
-			}
+			if (DB::query("SELECT * FROM clientProfile WHERE email ='".$email."'")) {
+				header("Location: ../../index.php?signup=email");
+			//check username
+			} elseif (DB::query("SELECT * FROM login WHERE UName ='".$uname."'")) {
+				header("Location: ../../index.php?signup=username");
+			} else {
+			//unknown issue
+        header("Location: ../../index.php?signup=fail");
+      }
 		} else {
+		  // Get Users
+			$result = DB::query("SELECT * FROM clientProfile WHERE email = '".$email."'");
+			foreach ($result as $row) {
+			 $cpid = $row['CPID'];
 
-			// Get Users
-			$result = myDB::getInstance()->getUser($email);
-			$row = $result->fetch_row();
-			$uid = $row[0];
 
 			// Add Login
-			myDB::getInstance()->addLogin($uid, $uname, $pwd);
+			DB::insert('login', array(
+        'LID' => NULL,
+        'CPID' => $cpid,
+        'UName' => $uname,
+        'PWD' => $hashedPWD
+      ));
 			header('location: ../../index.php?signup=success');
 			exit;
+      }
 		}
 	}
 ?>
